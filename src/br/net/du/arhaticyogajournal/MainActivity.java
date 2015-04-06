f7d56dbc4d8392d4d64b7bc5c47f7acddba9c297
Eduardo Sztokbant <du@du.net.br>
2015-04-05 20:44:26 -0700

    First functional version

--- src/br/net/du/arhaticyogajournal/MainActivity.java
+++ src/br/net/du/arhaticyogajournal/MainActivity.java
@@ -2,14 +2,36 @@ package br.net.du.arhaticyogajournal;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+	private WebView myWebView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		myWebView = (WebView) findViewById(R.id.webview);
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		myWebView.setWebViewClient(new WebViewClient());
+		myWebView.loadUrl("http://www.arhaticyogajournal.com");
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Check if the key event was the Back button and if there's history
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+			myWebView.goBack();
+			return true;
+		}
+		// If it wasn't the Back key or there's no web page history, bubble up to the default system behavior (probably
+		// exit the activity)
+		return super.onKeyDown(keyCode, event);
 	}
 }
